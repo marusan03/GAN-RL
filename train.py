9ab27df8e2499bdc17ea2573bc63e11a8ac0a9eb
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-11-08 13:48:41 +0900

    change batch size

--- train.py
+++ train.py
@@ -123,16 +123,16 @@ def train(sess, config):
         if step > config.learn_start:
             if step % config.train_frequency == 0:
                 s_t, action_batch, reward_batch, s_t_plus_1, terminal_batch = memory.sample()
-                if gats_s_t == []:
-                    gats_s_t = s_t
-                    gats_action_batch = action_batch
-                    gats_s_t_plus_1 = s_t_plus_1
-                else:
-                    gats_s_t = np.concatenate([gats_s_t, s_t], axis=0)
-                    gats_action_batch = np.concatenate(
-                        [gats_action_batch, action_batch], axis=0)
-                    gats_s_t_plus_1 = np.concatenate(
-                        [gats_s_t_plus_1, s_t_plus_1], axis=0)
+                # if gats_s_t == []:
+                #     gats_s_t = s_t
+                #     gats_action_batch = action_batch
+                #     gats_s_t_plus_1 = s_t_plus_1
+                # else:
+                #     gats_s_t = np.concatenate([gats_s_t, s_t], axis=0)
+                #     gats_action_batch = np.concatenate(
+                #         [gats_action_batch, action_batch], axis=0)
+                #     gats_s_t_plus_1 = np.concatenate(
+                #         [gats_s_t_plus_1, s_t_plus_1], axis=0)
 
                 q_t, loss, dqn_summary = agent.train(
                     s_t, action_batch, reward_batch, s_t_plus_1, terminal_batch, step)
@@ -147,8 +147,8 @@ def train(sess, config):
 
             if config.gats and step % config.gdm_train_frequency == 0:
                 gdm.summary, disc_summary = gdm.train(
-                    gats_s_t, np.reshape(
-                        gats_action_batch, [-1, 1]), np.reshape(gats_s_t_plus_1[:, 3, ...], [-1, 1, 84, 84]))
+                    s_t, np.reshape(
+                        action_batch, [-1, 1]), np.reshape(s_t_plus_1[:, 3, ...], [-1, 1, 84, 84]))
                 writer.add_summary(gdm.summary, step)
                 writer.add_summary(disc_summary, step)
 
