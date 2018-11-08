55bb7bc56b634985180037ec944982168c73e97c
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-11-08 13:16:52 +0900

    bug fix

--- train.py
+++ train.py
@@ -123,11 +123,16 @@ def train(sess, config):
         if step > config.learn_start:
             if step % config.train_frequency == 0:
                 s_t, action_batch, reward_batch, s_t_plus_1, terminal_batch = memory.sample()
-                gats_s_t = np.concatenate([gats_s_t, s_t], axis=0)
-                gats_action_batch = np.concatenate(
-                    [gats_action_batch, action_batch], axis=0)
-                gats_s_t_plus_1 = np.concatenate(
-                    [gats_s_t_plus_1, s_t_plus_1], axis=0)
+                if gats_s_t == []:
+                    gats_s_t = s_t
+                    gats_action_batch = action_batch
+                    gats_s_t_plus_1 = gats_s_t_plus_1
+                else:
+                    gats_s_t = np.concatenate([gats_s_t, s_t], axis=0)
+                    gats_action_batch = np.concatenate(
+                        [gats_action_batch, action_batch], axis=0)
+                    gats_s_t_plus_1 = np.concatenate(
+                        [gats_s_t_plus_1, s_t_plus_1], axis=0)
 
                 q_t, loss, dqn_summary = agent.train(
                     s_t, action_batch, reward_batch, s_t_plus_1, terminal_batch, step)
