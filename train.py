d84504f1e83121b52b1d3431b9f955a962abe177
e155763 <e155763@ie.u-ryukyu.ac.jp>
2018-11-30 23:28:41 +0900

    test

--- train.py
+++ train.py
@@ -143,7 +143,6 @@ def train(sess, config):
         screen, reward, terminal = env.act(action, is_training=True)
         reward = max(config.min_reward, min(config.max_reward, reward))
         history.add(norm_frame(screen))
-        print(reward)
         memory.add(screen, reward, action, terminal)
 
         # Train
