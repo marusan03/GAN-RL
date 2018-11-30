7a547e13156a1b86477927be28d7319560743496
e155763 <e155763@ie.u-ryukyu.ac.jp>
2018-11-30 17:27:04 +0900

    bug fix

--- rp.py
+++ rp.py
@@ -28,7 +28,7 @@ class RP():
         self.action = tf.placeholder(
             tf.int32, shape=[None, self.lookahead+1], name='actions')
         self.reward = tf.placeholder(
-            tf.float32, shape=[None, self.lookahead+1, self.num_rewards, self.num_rewards], name='rewards')
+            tf.float32, shape=[None, self.lookahead+1, self.num_rewards], name='rewards')
         self.state = tf.placeholder(
             tf.float32, shape=[None, self.history_length + self.lookahead, self.state_width, self.state_height], name='state')
 
@@ -99,7 +99,7 @@ class RP():
             outputs = self.predicted_reward[:,
                                             self.num_rewards * ind: self.num_rewards * (ind + 1)]
             loss = loss + \
-                tf.nn.softmax_cross_entropy_with_logits_v2(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(
                     labels=reward[:, ind, 0], logits=outputs)
 
         with tf.name_scope('weight_decay'):
