46e343c18e2b1ad3ceaba7c976cc55591a839135
e155763 <e155763@ie.u-ryukyu.ac.jp>
2018-11-30 10:08:55 +0900

    test

--- gdm.py
+++ gdm.py
@@ -53,11 +53,13 @@ class GDM():
                 self.pre_state, tf.expand_dims(self.action[:, 0], axis=1), self.is_training, ngf=self.gdm_ngf))
             self.predicted_state = tf.concat(
                 [self.pre_state, self.predicted_state], axis=1)
+            print(self.predicted_state.shape)
 
         with tf.variable_scope('gdm', reuse=True):
             for i in range(1, self.lookahead):
                 self.predicted_state = tf.concat([self.predicted_state, norm_state_Q_GAN(self.build_gdm(
                     self.pre_state[:, -1*self.history_length:, ...], tf.expand_dims(self.action[:, i], axis=1), self.is_training, ngf=self.gdm_ngf))], axis=1)
+                print(self.predicted_state.shape)
 
         with tf.name_scope('opt'):
             self.gdm_train_op, self.disc_train_op, self.gdm_summary, self.disc_summary = self.build_training_op(
@@ -285,8 +287,6 @@ class GDM():
             [pre_state, post_state], axis=self.concat_dim, name='real_state')
         fake_state = tf.concat(
             [pre_state, predicted_state], axis=self.concat_dim, name='fake_state')
-        print(pre_state.shape)
-        print(predicted_state.shape)
 
         with tf.name_scope('disc_fake'):
             with tf.variable_scope('discriminator'):
