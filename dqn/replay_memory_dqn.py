7e691ca9c62fb2d85f006315746b4ef9f007f797
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-12-12 22:40:32 +0900

    test

--- dqn/replay_memory_dqn.py
+++ dqn/replay_memory_dqn.py
@@ -144,8 +144,8 @@ class ReplayMemoryDQN:
         if self.lookahead == 1:
             actions = np.expand_dims(self.actions[indexes], axis=1)
         else:
-            actions = self.actions[[np.expand_dims(indexes, axis=0), np.expand_dims(
-                indexes + self.lookahead - 1, axis=1)]]
+            actions = [self.actions[i:i+self.lookahead-1]
+                       for i in range(indexes)]
 
         if self.cnn_format == 'NHWC':
             return np.transpose(self.gan_states[:, :self.history_length, ...], (0, 2, 3, 1)), actions, np.transpose(self.gan_states[:, self.history_length:, ...], (0, 2, 3, 1))
@@ -181,11 +181,8 @@ class ReplayMemoryDQN:
                 index, self.lookahead)
             indexes.append(index)
 
-        indexes = np.array(indexes)
-        actions = self.actions[[np.expand_dims(
-            indexes, axis=0), np.expand_dims(indexes + self.lookahead, axis=0)]]
-        rewards = self.rewards[[np.expand_dims(
-            indexes, axis=0), np.expand_dims(indexes + self.lookahead, axis=0)]]
+        actions = [self.actions[i:i+self.lookahead] for i in range(indexes)]
+        rewards = [self.rewards[i:i+self.lookahead] for i in range(indexes)]
 
         if self.cnn_format == 'NHWC':
             return np.transpose(self.prestates[:, :self.history_length, ...], (0, 2, 3, 1)), actions, rewards
