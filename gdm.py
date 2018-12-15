30b256d9ef40cfcaf10a17376e4908b8cfdeba40
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-12-15 16:42:04 +0900

    bug fix

--- gdm.py
+++ gdm.py
@@ -74,7 +74,8 @@ class GDM():
         actions = []
         for _ in range(num_rollout):
             action = [[random.randint(0, self.num_actions)]]
-            predicted_state = self.get_state(state, action)
+            predicted_state = self.sess.run(self.predicted_state, feed_dict={
+                self.pre_state: state, self.action: action, self.is_training: False})
             state = np.concatenate([state, predicted_state], axis=1)
             actions.append(action)
         return state, actions
