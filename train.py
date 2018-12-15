564eac3efd1d82a0ead8767140c121b5bb403d85
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-12-15 16:51:17 +0900

    bug fix

--- train.py
+++ train.py
@@ -349,8 +349,7 @@ def rollout_image(config, image_dir, gdm, state, step, num_rollout=4):
         np.expand_dims(state, axis=0), num_rollout)
     action_label = [str(action) for action in actions]
     action_label = '.'.join(action_label)
-    print(images.shape, images[0].shape)
-    pil_image = Image.fromarray(unnorm_frame(images))
+    pil_image = Image.fromarray(unnorm_frame(images[0]))
     pil_image.save(image_dir + 'rollout_{}_{}.jpg'.format(action_label, step))
 
 
