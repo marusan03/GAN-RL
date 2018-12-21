a2c7b588ec4bda4428a0aecc9266fca57575322c
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-12-21 15:06:46 +0900

    bug fix

--- dqn/environment.py
+++ dqn/environment.py
@@ -43,7 +43,7 @@ class Environment(object):
     @ property
     def screen(self):
         # return imresize(rgb2gray(self._screen), self.dims)
-        return imresize(rgb2gray(self._screen).mean(2), self.dims).reshape((1) + self.dims)
+        return imresize(rgb2gray(self._screen).mean(2), self.dims)
 
     @property
     def action_size(self):
