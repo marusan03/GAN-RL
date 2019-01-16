d7914fd65809fbca000539481b5a3b7acdfb2d4b
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2019-01-17 00:39:36 +0900

    test

--- gdm.py
+++ gdm.py
@@ -30,10 +30,10 @@ class GDM():
         self.lambda_l1 = self.config.lambda_l1
         self.lambda_l2 = self.config.lambda_l2
 
-        self.initializer = tf.random_normal_initializer(0.0, 0.02)
-        self.batch_norm_initializer = tf.random_normal_initializer(1.0, 0.02)
-        # self.initializer = None
-        # self.batch_norm_initializer = None
+        # self.initializer = tf.truncated_normal_initializer(0.0, 0.02)
+        # self.batch_norm_initializer = tf.random_normal_initializer(1.0, 0.02)
+        self.initializer = None
+        self.batch_norm_initializer = None
 
         self.action = tf.placeholder(
             tf.int32, shape=[None, self.lookahead], name='actions')
