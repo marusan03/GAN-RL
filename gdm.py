cac76e476082521c25b6a4131c3c446fbeae9521
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2019-01-17 14:11:59 +0900

    test

--- gdm.py
+++ gdm.py
@@ -34,9 +34,9 @@ class GDM():
 
         self.initializer = tf.truncated_normal_initializer(0.0, 0.02)
         self.beta_initializer = tf.zeros_initializer()
-        self.gamma_initializer = tf.random_normal_initializer(1.0, 0.02)
+        # self.gamma_initializer = tf.random_normal_initializer(1.0, 0.02)
         # self.initializer = None
-        # self.batch_norm_initializer = None
+        self.gamma_initializer = None
 
         self.action = tf.placeholder(
             tf.int32, shape=[None, self.lookahead], name='actions')
